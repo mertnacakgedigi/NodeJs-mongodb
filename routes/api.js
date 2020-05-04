b0b93ca9cd3fe63396ae509c900f5ecf865dc615
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-05-04 11:34:55 -0700

    index request

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controllers')
 
+// Request
+router.get('/request',ctrl.request.indexRequest)
 router.post('/request',ctrl.request.create)
 
 
