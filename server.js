7dfe26d61902f4c3cd6fe97ad96ff2ebb7fb681c
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-05-08 11:45:06 -0700

    heroku

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json())
 
 app.use(session({
     // Store the session in our DB
-    store: new MongoStore({ url: process.env.MONGO_URI }),
+    store: new MongoStore({ url: process.env.MONGODB_URI }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false, // Only create a session if a property has been added to the session
